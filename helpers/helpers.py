d2d9f4dea38674232dbd452b53fa5528e3797707
Sebastien Ollquist <sebastienollquist@Sebastiens-MacBook-Air.local>
2022-10-11 19:59:47 +0200

    clean and update up to problem 17

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from typing import Generator
 
 class Helpers:
     def __init__(self):
@@ -47,5 +48,87 @@ class Helpers:
         if a < 0 or b < 0:
             raise ValueError('Parameters cannot be negative.')
         return (a * b) / self.gcd(a, b)
+    
 
+    def read_grid(self) -> list:
+        """Reads the grid file and returns a list of lists of integers"""
+        with open('../files/grid11.txt', 'r') as f:
+            return [[int(num) for num in line.split(' ')] for line in f.readlines()]
+    
+
+    def get_number_of_factors(self, n) -> int:
+        """Returns a list of all factors of a given number n"""
+        if not isinstance(n, int):
+            raise TypeError('Parameter must be an integer.')
+        if n < 1:
+            raise ValueError('Parameter cannot be negative.')
+        num_factors = 0
+        for i in range(2, int(n ** 0.5)+1):
+            if n % i == 0:
+                num_factors += 1
+                if i != n/i:
+                    num_factors += 1
+        return num_factors
+
+
+    def get_triangular_numbers(self, n):
+        """Generates the first n triangular numbers"""
+        if not isinstance(n, int):
+            raise TypeError('Parameter must be an integer.')
+        if n < 1:
+            raise ValueError('Parameter cannot be negative.')
+        return (n*(n+1)//2 for n in range(1, n+1))
+    
+
+    def read_large_numbers(self):
+        """Reads the large numbers file and returns a list of integers"""
+        with open('../files/largenums13.txt', 'r') as f:
+            return (int(line) for line in f.readlines())
+    
+
+    def generate_collatz_sequence(self, n, cnt) -> tuple:
+        """Generates the collatz sequence for a given number n"""
+        if n == 1:
+            return (1, cnt)
+        if n % 2 == 0:
+            return self.generate_collatz_sequence(int(n/2), cnt+1)
+        return self.generate_collatz_sequence(3*n + 1, cnt+1)
+    
+
+    def factorial(self, n):
+        """Returns the factorial of a given number n"""
+        if not isinstance(n, int):
+            raise TypeError('Parameter must be an integer.')
+        if n < 1:
+            raise ValueError('Parameter cannot be negative.')
+        if n == 1:
+            return 1
+        return n * self.factorial(n-1)
+    
+
+    def numbers_to_19(self) -> list:
+        return ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    
+
+    def tens(self) -> list:
+        return ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    
+
+    def number_to_english(self, n) -> str:
+        """Returns the english representation of a given number n"""
+        if not isinstance(n, int):
+            raise TypeError('Parameter must be an integer.')
+        if n < 1:
+            raise ValueError('Parameter cannot be negative.')
+        if n < 20:
+            return self.numbers_to_19()[n]
+        if n < 100:
+            return self.tens()[n//10] + ('' if n % 10 == 0 else ' ' + self.number_to_english(n % 10))
+        if n < 1000:
+            return self.numbers_to_19()[n//100] + ' hundred' + ('' if n % 100 == 0 else ' and ' + self.number_to_english(n % 100))
+        if n < 1000000:
+            return self.number_to_english(n//1000) + ' thousand' + ('' if n % 1000 == 0 else ' ' + self.number_to_english(n % 1000))
+        if n < 1000000000:
+            return self.number_to_english(n//1000000) + ' million' + ('' if n % 1000000 == 0 else ' ' + self.number_to_english(n % 1000000))
+        return self.number_to_english(n//1000000000) + ' billion' + ('' if n % 1000000000 == 0 else ' ' + self.number_to_english(n % 1000000000))
         
--- solved_problems/easy_problems.py
+++ solved_problems/easy_problems.py
@@ -13,14 +13,16 @@ NB_ITERATIONS = 1_000_000
 
 # Timer decorator
 def timer(func):
-    def f(*args, **kwargs):
+    """Decorator to time a function"""
+    def wrapper(*args, **kwargs):
+        """Wrapper function"""
         start = time()
         rv = func(*args, **kwargs)
         end = time()
-        print('Elapsed time: {:.3f} [s]'.format(end-start)) # time in seconds
+        print(f'Elapsed time: {(end-start):.3f} [s]') # time in seconds
         #print('Elapsed time: {:.3f} [ms]'.format((end-start)*1000)) # time in milliseconds
         return rv
-    return f
+    return wrapper
 
 
 class EasyProblems:
@@ -31,12 +33,14 @@ class EasyProblems:
     # PROBLEM 1
     @timer
     def sum_of_multiples_of_three_and_five(self, n):
+        """Returns the sum of all multiples of 3 and 5 below n"""
         return sum((_ for _ in range(n) if _%3 == 0 or _%5 == 0))
 
 
     # PROBLEM 2
     @timer
     def even_fibonacci(self, n):
+        """Returns the sum of all even fibonacci numbers up to n"""
         fib_arr = [1, 1]
         while fib_arr[-1] < n:
             fib_arr.append(fib_arr[-1]+fib_arr[-2])
@@ -46,6 +50,7 @@ class EasyProblems:
     # PROBLEM 3
     @timer
     def largest_prime_factor(self, n):
+        """Returns the largest prime factor of a given number n"""
         assert n > 0
         factors = []
         max_range = int(sqrt(n))
@@ -59,12 +64,14 @@ class EasyProblems:
     # PROBLEM 4
     @timer
     def largest_palindrome_product(self):
+        """Returns the largest palindrome made from the product of two 3-digit numbers"""
         return max([i*j for i in range(100, 1000) for j in range(100, 1000) if str(i*j)[::-1]==str(i*j)[::1]])
 
 
     # PROBLEM 5
     @timer
     def smallest_multiple(self, n):
+        """Returns the smallest positive number that is evenly divisible by all of the numbers from 1 to n"""
         res = 1
         for i in range(2, n+1):
             if help.is_prime(i):
@@ -75,6 +82,7 @@ class EasyProblems:
     # PROBLEM 6
     @timer
     def sum_square_difference(self, n):
+        """Returns the difference between the sum of the squares of the first n natural numbers and the square of the sum"""
         r = range(1, n+1)
         return abs(sum(_ ** 2 for _ in r) -  sum(r)**2)
     
@@ -82,6 +90,7 @@ class EasyProblems:
     # PROBLEM 7 - TODO: work on niceness
     @timer
     def get_prime(self, idx: int) -> int:
+        """Returns the idx-th prime number"""
         assert(idx > 0)
         if idx == 1:
             return 2
@@ -96,6 +105,7 @@ class EasyProblems:
     # PROBLEM 8
     @timer
     def largest_product_in_series(self, num, num_digits) -> int:
+        """Returns the largest product of num_digits consecutive digits in num"""
         prods = []
         for i in range(0, len(num)-num_digits):
             prods.append(prod(int(num[i+j]) for j in range(num_digits)))
@@ -105,6 +115,7 @@ class EasyProblems:
     # PROBLEM 9
     @timer
     def find_pythagorean_triplets(self) -> int:
+        """Find the product of the pythagorean triplet where a+b+c=1000"""
         gen = ((a,b,c) for a in range(1000) for b in range(1000) for c in range(1000) if a < b and b < c and a+b+c == 1000)
         for a,b,c in gen:
             if help.is_pythagorean_triplet(a,b,c):
@@ -115,10 +126,85 @@ class EasyProblems:
     # PROBLEM 10
     @timer
     def sum_primes_below(self, upper_bound) -> int:
+        """Sum of primes below upper_bound"""
         primes = (_ for _ in range(3, upper_bound, 2) if help.is_prime(_))
         return 2 + reduce(lambda x,y: x+y, primes) # works as fast using the sum() function
+    
+
+    # PROBLEM 11
+    @timer
+    def largest_product_in_grid(self, grid, num_numbers) -> int:
+        """Find the largest product of num_numbers numbers in a grid"""
+        max_prod = 0
+        # horizontal
+        for i,el in enumerate(grid):
+            for j in range(len(el)-num_numbers):
+                max_prod = max(max_prod, prod(el[j:j+num_numbers]))
+        # vertical
+        for i in range(len(grid)-num_numbers):
+            for j in range(len(grid[i])):
+                max_prod = max(max_prod, prod(grid[i+k][j] for k in range(num_numbers)))
+        # diagonal
+        for i in range(len(grid)-num_numbers):
+            for j in range(len(grid[i])-num_numbers):
+                max_prod = max(max_prod, prod(grid[i+k][j+k] for k in range(num_numbers)))
+        # anti-diagonal
+        for i in range(len(grid)-num_numbers):
+            for j in range(len(grid[i])-num_numbers):
+                max_prod = max(max_prod, prod(grid[i+k][j-k] for k in range(num_numbers)))
+        return max_prod
+    
+
+    # PROBLEM 12
+    @timer
+    def highly_divisible_triangular_number(self, n) -> int:
+        """Returns the first triangular number with more than n divisors"""
+        for tn in help.get_triangular_numbers(n):
+            if help.get_number_of_factors(tn) > 500:
+                return tn
+    
+    
+    # PROBLEM 13
+    @timer
+    def large_sum(self) -> int:
+        """Sums the first 10 digits of the sum of the numbers in the file"""
+        return str(sum(help.read_large_numbers()))[:10]
+
+
+    # PROBLEM 14
+    @timer
+    def longest_collatz_sequence(self, upper_bound) -> int:
+        """Returns the number under upper_bound that produces the longest Collatz sequence"""
+        max_len, max_num = 0, 0
+        for i in range(3, upper_bound, 2):
+            _, _len = help.generate_collatz_sequence(i, 0)
+            if _len > max_len:
+                max_len = _len
+                max_num = i
+        return max_num
+    
+
+    # PROBLEM 15
+    @timer
+    def lattice_paths(self, n) -> int:
+        """Compute the number of paths in an n x n grid"""
+        return int(help.factorial(2*n) / (help.factorial(n)**2))
+    
+
+    # PROBLEM 16
+    @timer
+    def power_digit_sum(self, n) -> int:
+        """Compute the sum of the digits of 2^n"""
+        return sum(int(_) for _ in str(2**n))
+    
+
+    # PROBLEM 17
+    @timer
+    def number_letter_counts(self, n) -> int:
+        """Compute the number of letters in the numbers 1 to n, ignoring the spaces"""
+        return sum(len(help.number_to_english(_).replace(' ', '')) for _ in range(1, n+1))
 
 
 if __name__ == '__main__':
     ep = EasyProblems()
-    print(ep.sum_square_difference(NB_ITERATIONS))
+    print(ep.number_letter_counts(1000))
